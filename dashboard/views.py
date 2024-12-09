928f35d839e40869f959966f30f8883ab79ba1d5
Jefte <jeftefontes@gmail.com>
2024-12-09 13:56:02 -0300

    mudança na view

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,9 +8,6 @@ from .models import CoordenadorExtensao
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import EstagiarioCadastroForm, EmpresaCadastroForm
-from django.shortcuts import get_object_or_404
-from .models import CoordenadorExtensao, Estagio
-from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'dashboard/home.html')
 
