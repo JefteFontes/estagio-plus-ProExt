73fb415fe382ddd7b16be5e824e0d6070243fbab
Hyann <hyanngabriels@gmail.com>
2025-03-30 17:08:58 -0300

    smtp reset password

--- dashboard/views/home.py
+++ dashboard/views/home.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 import pdfplumber
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from dashboard.models import (
     Empresa,
     Endereco,
